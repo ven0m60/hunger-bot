52e01b6c13c08f64808059f2e1eddfaee79e9b2d
ven0m60 <zach.anthony@me.com>
2017-11-16 16:00:28 +1100

    Updated filtering of AI array

--- src/dialog/ai.ts
+++ src/dialog/ai.ts
@@ -4,14 +4,19 @@ import * as send from "../messenger/send";
 const app = apiai(process.env.AI_TOKEN);
 
 export let textRequest = (text: string, sender: string) => {
-  send.typingOn(sender);
   const request = app.textRequest(text, {
     sessionId: sender
   });
+
   request.on("response", (response) => {
     console.log(response);
-    const message: string = response.result.fulfillment.messages.filter(retrieveText);
-    const customPayload: any = response.result.fulfillment.messages.filter(retrieveCustomPayload);
+
+    const messages: any = response.result.fulfillment.messages.filter(retrieveText);
+    const message: string = messages[0].speech;
+
+    const customPayloads: any = response.result.fulfillment.messages.filter(retrieveCustomPayload);
+    const customPayload: any = customPayloads[0].payload;
+
     if (!message && !customPayload) {
       const err = new Error("Invalid response from AI");
       throw err;
@@ -22,28 +27,13 @@ export let textRequest = (text: string, sender: string) => {
   request.on("error", (error) => {
   console.log(error);
   });
-  send.typingOff(sender);
   request.end();
 };
 
-export let retrieveText = (messages: any): string => {
-  console.log(messages);
-  const text: any = messages.type === 0 && !messages.hasOwnProperty("platform") && messages.hasOwnProperty("speech");
-  console.log("text: ", text.length);
-  if (text.length <= 1) {
-    return text[0].speech;
-  } else {
-    const err = new Error("Invalid response from AI");
-    throw err;
-  }
+export let retrieveText = (messages: any): any => {
+  return messages.type === 0 && !messages.hasOwnProperty("platform") && messages.hasOwnProperty("speech");
 };
 
 export let retrieveCustomPayload = (messages: any): any => {
-  const payloadBody: any = messages.type === 4 && !messages.hasOwnProperty("platform") && messages.hasOwnProperty("payload");
-  if (payloadBody.length <= 1) {
-    return payloadBody[0].payload;
-  } else {
-    const err = new Error("Invalid response from AI");
-    throw err;
-  }
+  return messages.type === 4 && !messages.hasOwnProperty("platform") && messages.hasOwnProperty("payload");
 };
